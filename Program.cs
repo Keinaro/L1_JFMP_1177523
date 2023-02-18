c62cee8f3dcf581fcdce7431261b9db856aa6afb
georg <georg@LAPTOP-FAM-MUNO>
2023-02-18 11:21:51 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo, soy George");
+
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("soy George");
+
+        Console.Write("Hola Mundo");
+        Console.Write(" soy George\n");
+
+        Console.WriteLine("\nIngrese su nombre: ");
+        string nombre = Console.ReadLine();
+
+        Console.Write("Hola Mundo ");
+        Console.Write("soy " + nombre);
+        Console.ReadKey();
+    }
+}
